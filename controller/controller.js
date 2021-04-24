34cea6421da0f931e8171aad421141ca40cd76b0
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2021-04-24 21:52:36 +0300

    Ajout service

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,19 @@
+var connexion = require('../model/connexion')();
+
 module.exports = {
     index: function(req, res){
-        res.send("Hello world!");
+        console.log("==> GET INDEX");
+        res.send("Bonjour!");
     },
+
+    list: function(req, res){
+        console.log("==> GET LIST");
+        connexion.then(function(dbo){
+            dbo.collection("user").find({}, { projection: { _id: 0, nom: 1, prenom: 1 } }).toArray(function(err, resultats){
+                if(err) res.status(500).send("Error: ressource");
+                console.log("==> FIN GET LIST");
+                res.status(200).send(resultats);
+            })
+        })
+    }
 }
--- router/route.js
+++ router/route.js
@@ -4,4 +4,5 @@ var middleware = require("../middleware/auth");
 // On injecte le router d"express, nous en avons besoin pour définir les routes 
 module.exports = function(router) {   
     router.get("/", controller.index);
+    router.get("/list", controller.list);
 };
